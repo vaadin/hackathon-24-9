6f51b476a01fb1b974cc75108ad43be727afcc3f
start.vaadin.com <no-reply@vaadin.com>
2025-09-12 07:56:27 +0000

    Generated project

---
+++ src/main/java/com/koodivaara/taskmanagement/TaskService.java
@@ -0,0 +1,36 @@
+package com.koodivaara.taskmanagement;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Transactional
+    public void createTask(String description, @Nullable LocalDate dueDate) {
+        if ("fail".equals(description)) {
+            throw new RuntimeException("This is for testing the error handler");
+        }
+        var task = new Task(description, Instant.now());
+        task.setDueDate(dueDate);
+        taskRepository.saveAndFlush(task);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Task> list(Pageable pageable) {
+        return taskRepository.findAllBy(pageable).toList();
+    }
+
+}
